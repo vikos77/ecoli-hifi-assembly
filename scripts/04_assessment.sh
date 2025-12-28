36fa1f3f32640833275532aa63e2d9c1b270e503
vikos77 <vigneshwaran0594@gmail.com>
2025-12-28 12:47:46 +0000

    Add environment file

--- scripts/04_assessment.sh
+++ scripts/04_assessment.sh
@@ -4,17 +4,10 @@
 
 #Step 1
 
-mkdir ../results/quast_report
-
 #We use quast tool to analyse the assembled data 
-quast.py ../results/assembly/ecoli_assembly.fasta -o ../results/quast_report/quast_results
+quast.py ../results/assembly/ecoli_assembly.fasta --glimmer -o ../results/assembly/quast_results
 
 #Quast gives the statistical data of the contig assembled such as N50 and L50 along with the GC content which could be used to infer the
 # quality of the assembly
 
 #Step 2
-
-busco -i ecoli_assembly.fasta \
-	-l bacteria_odb10 \
-	-o busco_results \
-	-m genome
